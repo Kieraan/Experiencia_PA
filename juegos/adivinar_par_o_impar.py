60239bdb7cb4a37e19b02b0309a2d2d73d6033e1
PEDRO MUNITA <pedro.munita@uc.cl>
2024-03-14 10:48:53 -0300

    adivinar_par

--- juegos/adivinar_par_o_impar.py
+++ juegos/adivinar_par_o_impar.py
@@ -9,8 +9,35 @@ def adivinar_par_o_impar():
 
     """
 
+    numero_random = random.randint(1, 10)
+    print("¿Es el número par o impar?")
     print("escoja su opción : (impar=1 ; par=2)")
     opcion = int(input()) 
+    
 
+
+    if numero_random%2 == 0: #par
+        if opcion == 1:
+            print("NO ADIVINASTEE")
+
+        elif opcion == 2:
+            print("Adivinaste correctamente")
+        else:
+            print("escoge una opción válida >:( todo de nuevo")
+            adivinar_par_o_impar()
+
+
+    else: #imparpar
+        if opcion == 1:
+            print("ADIVINASTEE")
+
+        elif opcion == 2:
+            print("No Adivinaste correctamente")
+        else:
+            print("escoge una opción válida >:( todo de nuevo")
+            adivinar_par_o_impar()
+
+
+adivinar_par_o_impar()
 
     
