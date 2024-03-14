b30dbfd50c703d2c6c85c9d8c69744cabffeb86c
Kieraan <kieran1hconradie@gmail.com>
2024-03-14 10:33:51 -0300

    Implementación inicial

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def juego_del_dado():
     """
     Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
@@ -5,4 +7,19 @@ def juego_del_dado():
     Después el computador también tiene que lanzar un dado.
     El primero en sumar 30 puntos gana.
     """
-    pass
+    puntos_jugador = 0
+    puntos_cpu = 0
+    while (puntos_jugador and puntos_jugador) < 30:
+        input('Presione enter')
+        # El jugador recibe puntos
+        puntos_jugador += randint(1, 6)
+        puntos_cpu += randint (1, 6)
+        print(f'Puntos jugador: {puntos_cpu} \n Puntos CPU: {puntos_cpu}')
+        
+    if puntos_jugador >= 30:
+        print('Jugador Gana')
+    
+    else:
+        print('CPU gana')
+        
+    return
